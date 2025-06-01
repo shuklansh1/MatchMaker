16d17d4069c369a80645cb75fbf8edcc9d55c402
shuklansh <anshdpsviib201213@gmail.com>
2025-06-01 12:49:17 +0530

    cleanup

--- app/src/main/java/com/example/matchmaker/MainActivity.kt
+++ app/src/main/java/com/example/matchmaker/MainActivity.kt
@@ -33,52 +33,8 @@ class MainActivity : ComponentActivity() {
                         )
                     )
                     RecyclerViewComponent(people)
-                    /*AndroidView(
-                        factory = { ctx ->
-                            RecyclerView(ctx).apply {
-                                layoutManager = LinearLayoutManager(ctx)
-                                adapter = PersonAdapter(
-                                    people
-                                )
-                                setHasFixedSize(true)
-                            }
-                        },
-                    )*/
                 }
             }
         }
     }
 }
-
-/*class PersonAdapter(private val people: List<Person>) :
-    RecyclerView.Adapter<PersonAdapter.PersonViewHolder>() {
-
-    class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(com.example.ui.R.id.tvName)
-        val age: TextView = itemView.findViewById(com.example.ui.R.id.tvAge)
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): PersonViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_person, parent, false)
-        return PersonViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
-        val person = people[position]
-        holder.name.text = person.name
-        holder.age.text = "Age: ${person.age}"
-    }
-
-    override fun getItemCount(): Int = people.size
-}*/
-
-/*
-data class Person(
-    val name: String,
-    val age: Int,
-    val email: String
-)*/
--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -46,14 +47,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
